4e5882c8f349eabe6f90ee23f9e4b9d07a9a6aab
Luc Somers <lucsomers@hotmail.com>
2020-05-21 11:56:23 +0200

    Added navmesh and agent

---
+++ TeenageSimulator/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public Camera cam;
+
+    public LayerMask MovementMask;
+    public NavMeshAgent agent;
+
+    public Transform Player;
+
+    private bool mouseDown = false;
+    private float MovementWait = 0.5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseDown = Input.GetMouseButtonDown(1);
+
+        if (mouseDown)
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, MovementMask))
+            {
+                StopAllCoroutines();
+                //move our player to hit
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                //StartCoroutine(MoveToPoint(hit.point));
+                agent.destination = hit.point;
+            }
+        }
+    }
+
+    IEnumerator MoveToPoint(Vector3 pointToMoveTo)
+    {
+        Vector3 LerpVector = Vector3.zero;
+
+        while (Player.position != pointToMoveTo )
+        {
+            LerpVector = Vector3.Lerp(Player.position, pointToMoveTo, Time.deltaTime);
+
+            Player.position = LerpVector;
+            yield return null;
+        }
+    }
+}
