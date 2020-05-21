053ee97bb41bcd06fa062cf9c0bcba337c54a380
Luc Somers <lucsomers@hotmail.com>
2020-05-21 13:31:37 +0200

    Fixed movement

---
+++ TeenageSimulator/Assets/FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    [Header("Offsets")]
+    [Range(-100, 100)]
+    public float zOffset = 0;
+    [Range(-100, 100)]
+    public float xOffset = 0;
+
+    private Transform cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.position = new Vector3(player.position.x + xOffset, cam.position.y, player.position.z + zOffset);
+    }
+}
