776f0a039fd8f7914f1cbc605f870bb6a92d35f5
Luc Somers <lucsomers@hotmail.com>
2020-05-21 10:43:50 +0200

    First try raycast movement

---
+++ TeenageSimulator/Assets/PlayerMovementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public Camera cam;
+
+    bool mouseDown = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseDown = Input.GetMouseButtonDown(0);
+
+        if (mouseDown)
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //move our player to hit
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                print("We hit " + hit.collider.name + " " + hit.point);
+            }
+        }
+    }
+}
