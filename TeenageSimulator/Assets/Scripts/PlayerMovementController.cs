ca96af0bddc3ced2cb2e7b49afa98b939004e8bf
Luc Somers <lucsomers@hotmail.com>
2020-05-21 11:59:49 +0200

    Clean code

--- TeenageSimulator/Assets/Scripts/PlayerMovementController.cs
+++ TeenageSimulator/Assets/Scripts/PlayerMovementController.cs
@@ -13,7 +13,6 @@ public class PlayerMovementController : MonoBehaviour
     public Transform Player;
 
     private bool mouseDown = false;
-    private float MovementWait = 0.5f;
 
 
     // Update is called once per frame
@@ -28,25 +27,9 @@ public class PlayerMovementController : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, 100, MovementMask))
             {
-                StopAllCoroutines();
                 //move our player to hit
-                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
-                //StartCoroutine(MoveToPoint(hit.point));
                 agent.destination = hit.point;
             }
-        }
-    }
-
-    IEnumerator MoveToPoint(Vector3 pointToMoveTo)
-    {
-        Vector3 LerpVector = Vector3.zero;
-
-        while (Player.position != pointToMoveTo )
-        {
-            LerpVector = Vector3.Lerp(Player.position, pointToMoveTo, Time.deltaTime);
-
-            Player.position = LerpVector;
-            yield return null;
         }
     }
 }
