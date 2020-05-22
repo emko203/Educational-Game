fab01f7dd450f8159d7efb528c5cdea8406860bd
Luc Somers <lucsomers@hotmail.com>
2020-05-22 10:34:03 +0200

    BakedLightmap

---
+++ TeenageSimulator/Assets/Scripts/Interactable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [Range(1,100)]
+    public float radius = 3f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position,radius);
+    }
+}
