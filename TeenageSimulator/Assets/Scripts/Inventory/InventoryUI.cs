4a54c8b25e20672586eec0d1bdd5836baf452cfe
Karapachov,Emanuil E.C <e.karapachov@student.fontys.nl>
2020-06-04 14:25:20 +0200

    InventorySystem
    Added a scene with a placeholder inventory, item scriptable objects, and a logic for the picking up of items

---
+++ TeenageSimulator/Assets/Scripts/Inventory/InventoryUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public Transform itemsParent;
+    public GameObject inventoryUI;
+
+    InventorySlot[] slots;
+
+    Inventory inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallback += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Has to be changed later on
+        if (Input.GetButtonDown("Inventory")) 
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+    }
+
+    void UpdateUI() 
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
---
+++ TeenageSimulator/Assets/Scripts/Inventory/ItemPickupInteractable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ItemPickupInteractable : Interactable
+{
+    public Item item;
+
+    public override void HandleInteraction()
+    {
+        PickUp();
+    }
+
+
+    public void PickUp() 
+    {
+        Debug.Log("Picking up " + item.name);
+        //Add to inventory
+        bool wasPickedUp = Inventory.instance.Add(item);
+        if (wasPickedUp)
+        {
+            Destroy(gameObject);
+        }        
+    }
+}
