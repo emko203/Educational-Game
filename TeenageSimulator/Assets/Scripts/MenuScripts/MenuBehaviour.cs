bde3f6ea54e7167497bf24a008af0b5da8644725
Wesley <wesley.verhagen@student.fontys.nl>
2020-06-04 10:23:53 +0200

    Added main menu logic

---
+++ TeenageSimulator/Assets/Scripts/MenuScripts/MenuBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public void LoadScene(string targetScene)
+    {
+        SceneManager.LoadScene(targetScene);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
