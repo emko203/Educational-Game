b40d90175f4fde12c0bf1d8848aadea84c4ef53b
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-06-11 15:29:13 +0200

    Flink veel zooi

---
+++ TeenageSimulator/Assets/Scripts/Hover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    void OnMouseOver()
+    {
+        Debug.Log("Mouse is over GameObject.");
+        gameObject.GetComponent<Renderer>().material.color = new Color(1.5f, 1.5f, 1.5f); 
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("Mouse is no longer on GameObject.");
+        gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f); 
+    }
+}
