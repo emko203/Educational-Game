1ba9a66b98280efadad287719a75914d74e86e8f
Luc Somers <lucsomers@hotmail.com>
2020-06-11 11:55:09 +0200

    Added trigger interactable

---
+++ TeenageSimulator/Assets/Scripts/TriggerInteractable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class TriggerInteractable : DialogInteractable
+{
+    private void Awake()
+    {
+        setRangeOfCollider();
+    }
+
+    private void setRangeOfCollider()
+    {
+        SphereCollider collider = GetComponent<SphereCollider>();
+
+        collider.radius = this.radius;
+    }
+
+    public override void EndInteraction()
+    {
+        base.EndInteraction();
+    }
+
+    public override void HandleInteraction()
+    {
+        base.HandleInteraction();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerMotor player = other.gameObject.GetComponent<PlayerMotor>();
+
+        if (player != null)
+        {
+            HandleInteraction();
+            player.StopMoving();
+        }
+    }
+}
