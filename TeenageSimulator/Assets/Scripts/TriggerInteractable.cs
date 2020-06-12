dd444e06355a3a36df9826d299a49e4261efa12d
Luc Somers <lucsomers@hotmail.com>
2020-06-12 12:00:14 +0200

    Added a trigger to mister witkamp

--- TeenageSimulator/Assets/Scripts/TriggerInteractable.cs
+++ TeenageSimulator/Assets/Scripts/TriggerInteractable.cs
@@ -23,10 +23,10 @@ public class TriggerInteractable : DialogInteractable
 
     private void setRangeOfCollider()
     {
-        SphereCollider collider = GetComponent<SphereCollider>();
+        //SphereCollider collider = GetComponent<SphereCollider>();
 
-        collider.radius = this.radius;
-        collider.isTrigger = true;
+        //collider.radius = radius;
+        //collider.isTrigger = true;
     }
 
     private void OnTriggerEnter(Collider other)
