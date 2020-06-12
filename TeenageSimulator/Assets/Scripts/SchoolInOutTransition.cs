a6c2ab559153d916019b7b3e6b4c794774c89f69
Karapachov,Emanuil E.C <e.karapachov@student.fontys.nl>
2020-06-12 09:59:42 +0200

    Added Transition from school to outdoors

---
+++ TeenageSimulator/Assets/Scripts/SchoolInOutTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SchoolInOutTransition : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("character") && !other.isTrigger)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
