8bf6b38ea39dc3f91f9b9022344066c1d7d6ee3e
Luc Somers <lucsomers@hotmail.com>
2020-06-12 10:29:43 +0200

    Prefab situation sorted

---
+++ TeenageSimulator/Assets/BillBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    private Transform cam;
+
+    private void Start()
+    {
+        cam = Camera.current.transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(cam.position);
+    }
+}
