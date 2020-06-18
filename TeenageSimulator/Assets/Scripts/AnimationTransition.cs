dfed8a70a1c90d525c7905800750fc300fe93190
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-06-18 12:33:27 +0200

    Oops

---
+++ TeenageSimulator/Assets/Scripts/AnimationTransition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTransition : MonoBehaviour
+{
+    public void WalkingAnimation() 
+    { 
+    
+    }
+}
