a5bf4c9d489b4b244365935cfbbc2bae3f17f983
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-06-19 13:45:04 +0200

    Sit lol

---
+++ TeenageSimulator/Assets/Scripts/AnimationInteractable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationInteractable : Interactable
+{
+    public Animator animator;
+
+    public override void HandleInteraction()
+    {
+
+        base.HandleInteraction();
+    }
+
+    public override void EndInteraction()
+    {
+        base.EndInteraction();
+    }
+}
---
+++ TeenageSimulator/Assets/Scripts/SitAnimationInteractable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SitAnimationInteractable : AnimationInteractable
+{
+    //private GameObject getPlayer() 
+    //{ 
+    //    ge
+    //}
+
+    public GameObject player;
+    private Transform tf;
+    private void Start()
+    {
+        tf = player.GetComponent<Transform>();
+    }
+
+    public override void HandleInteraction()
+    {
+        tf.transform.position = new Vector3(3, 3, 3);
+        animator.SetBool("isSitting", true);
+        base.HandleInteraction();
+    }
+
+    public override void EndInteraction()
+    {
+        animator.SetBool("isSitting", false);
+        base.EndInteraction();
+    }
+
+
+}
