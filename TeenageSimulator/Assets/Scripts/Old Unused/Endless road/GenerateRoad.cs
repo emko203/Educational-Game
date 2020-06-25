820d81dcf51760b727b743584642907e3fe69fb0
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-06-25 08:25:58 +0200

    Fixed road

--- TeenageSimulator/Assets/Scripts/Old Unused/Endless road/GenerateRoad.cs
+++ TeenageSimulator/Assets/Scripts/Old Unused/Endless road/GenerateRoad.cs
@@ -1,32 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.XR.WSA.Input;
 
 public class GenerateRoad : MonoBehaviour
 {
     private IEnumerator coroutine;
     public GameObject road;
     public float interval;
-
-    private List<GameObject> tiles = new List<GameObject>();
-
+    GameObject clone;
+    public List<GameObject> tiles = new List<GameObject>();
+    private float pos = 0f;
     void Start()
     {
+        initialRoads();
         coroutine = WaitAndInstantiate(interval);
         StartCoroutine(coroutine);
     }
 
+    private void initialRoads() 
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            clone = Instantiate(road, new Vector3(pos, 0, 0), Quaternion.identity);
+            tiles.Add(clone);
+            pos += 50.4f;
+        }
+        pos -= 50.4f;
+    }
+  
+
+    private void Update()
+    {
+        tiles = tiles.Where(item => item != null).ToList();
+    }
+
     private IEnumerator WaitAndInstantiate(float waitTime)
     {
-        float pos = 0;
         while (true)
-        {          
-            yield return new WaitForSeconds(waitTime);
-            if(tiles.Count < 10)
+        {
+            yield return new WaitForSeconds(waitTime);               
+            pos += 50.4f;
+            clone = Instantiate(road, new Vector3(pos, 0, 0), Quaternion.identity);
+            tiles.Add(clone);
+            if (tiles.Count > 5)
             {
-                pos += road.GetComponent<Tile>().GetSpawnPosition().position.z;
-                GameObject clone = Instantiate(road, new Vector3(pos, 0, 0), Quaternion.identity);
-                tiles.Add(clone);
+                yield return new WaitForSeconds(waitTime);
+                Destroy(tiles[0]);
             }
         }
     }
---
+++ TeenageSimulator/Assets/Scripts/SkipText.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkipText : MonoBehaviour
+{
+    public TMP_Text nameplate;
+    private TMP_Text buttonText;
+    private Button button;
+    public int position = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        buttonText = GetComponentInChildren<TMP_Text>();
+        button.onClick.AddListener(Add);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        IntroText();
+        Debug.Log(position);
+    }
+
+    void Add() 
+    {
+        position++;
+    }
+
+    void IntroText()
+    {
+        switch (position)
+        {
+            case 1:
+                nameplate.text = "1";
+                break;
+            case 2:
+                nameplate.text = "2";
+                break;
+            case 3:
+                nameplate.text = "3";
+                break;
+            case 4:
+                nameplate.text = "4";
+                break;
+            case 5:
+                nameplate.text = "5";
+                buttonText.text = "Start Peter's journey";
+                break;           
+        }
+    }
+}
