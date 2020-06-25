0b6390bd995e261c4658c0c2c099930e3ad4b3d1
Karapachov,Emanuil E.C <e.karapachov@student.fontys.nl>
2020-06-25 13:12:59 +0200

    End Scene WIP

---
+++ TeenageSimulator/Assets/Scripts/Scene Specific Behaviour/End Scene/Open_Close_Info.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_Close_Info : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject TitleH;
+    private bool panelOpen = false;
+
+    public void OpenPanel() 
+    {
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+
+    public void ShowHappiness() 
+    {
+        OpenPanel();
+        TitleH.SetActive(true);
+    }
+
+    public void ShowBully() 
+    {
+        OpenPanel();
+        TitleH.SetActive(false);
+    }
+}
