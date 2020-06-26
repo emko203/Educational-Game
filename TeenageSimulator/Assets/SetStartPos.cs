a97349d972e4fe9295364e5275cc9b02b279c481
Luc Somers <lucsomers@hotmail.com>
2020-06-26 09:09:36 +0200

    Bagpackscene ended

---
+++ TeenageSimulator/Assets/SetStartPos.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartPos : MonoBehaviour
+{
+    [SerializeField]
+    Transform StartPosToSpawnAt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = StartPosToSpawnAt.position;
+    }
+}
