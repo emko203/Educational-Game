28227257e70878770b6146d7afac7259de503a5f
Wesley <wesley.verhagen@student.fontys.nl>
2020-06-26 09:49:52 +0200

    Stat changes now persistent between scenes

--- TeenageSimulator/Assets/Scripts/DialogSystem/StatHolder/StatHolderObject.cs
+++ TeenageSimulator/Assets/Scripts/DialogSystem/StatHolder/StatHolderObject.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "StatHolder", menuName = "StatSystem")]
 public class StatHolderObject : ScriptableObject
 {
+    [SerializeField]
+    private float StartHappiness;
+    [SerializeField]
+    private float StartBullyLevel;
+
     public float Happiness;
     public float BullyLevel;
+
+    
+    private void OnEnable()
+    {
+        Happiness = StartHappiness;
+        BullyLevel = StartBullyLevel;
+    }
 }
